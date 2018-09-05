b0b66e3e986fa49971e2636edecd2b14fdb246b7
zxinli <18266634989@163.com>
2018-09-05 16:02:35 +0800

    Update IDMPhoto.h

--- Classes/IDMPhoto.h
+++ Classes/IDMPhoto.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "IDMPhotoProtocol.h"
-#import <SDWebImage/SDWebImageManager.h>
+#import "SDWebImageManager.h"
 
 // This class models a photo/image and it's caption
 // If you want to handle photos, caching, decompression
